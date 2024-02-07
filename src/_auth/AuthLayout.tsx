2bd4616332c7a919e683b35c20f925100d65b70e
Shivam Patel <shivamp1804@gmail.com>
2024-02-08 01:19:50 +0530

    sign up form created

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,7 +12,10 @@ const AuthLayout = () => {
         <Outlet />
       </section>
 
-      <img src=""
+      <img src="/assets/images/side-img.svg"
+          alt="logo"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+      />
 
       </>
     )
